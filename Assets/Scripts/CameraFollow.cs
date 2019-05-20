58fc84fa71b2b40f28a97b5ef6191e741432ca43
Jiarong Gu <jiaronggu@hotmail.com>
2019-05-20 11:37:54 +0900

    added camera follow

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -5,27 +5,40 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public string defaultFollow;
+    public GameBoundary gameBoundary;
 
     private Vector3 defaultPosition;
+
+    private float height;
+
+    private float width;
 
     private Vector3 velocity = Vector3.zero;
 
     void Start()
     {
         defaultPosition = transform.position;
+        height = 2f * Camera.main.orthographicSize;
+        width = height * Camera.main.aspect;
     }
 
     void Update()
     {
         var followObject = FindObjectOfType<SpaceShip>();
 
-        if (followObject == null) {
+        if (followObject == null)
+        {
             transform.position = Vector3.SmoothDamp(transform.position, defaultPosition, ref velocity, 0.5f);
             return;
         }
 
         var position = followObject.transform.position;
-        transform.position = new Vector3(position.x, position.y, transform.position.z);
+        var bounds = gameBoundary.GetComponent<Renderer>().bounds;
+
+        transform.position = new Vector3(
+            Mathf.Clamp(position.x, bounds.min.x + width / 2, bounds.max.x - width / 2),
+            Mathf.Clamp(position.y, bounds.min.y + height / 2, bounds.max.y - height /2),
+            transform.position.z
+        );
     }
 }

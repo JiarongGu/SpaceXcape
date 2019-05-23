70a073b03fb1c472d7c0cde638e3aa35c922e218
Alice Guo <guoxiyuan593@gmail.com>
2019-05-24 09:53:36 +1000

    add script meteorite, add Level 4

---
+++ Assets/Scripts/Objects/meteorite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+public class meteorite : MonoBehaviour, IObjectCollider
+{
+    public meteorite crashMeteorite;
+
+    public Vector3 Center { get; set; }
+
+    public Rigidbody Rigidbody { get; set; }
+
+    public float Radius { get; set; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectCollider.Initalize(this);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        var spaceship = collision.gameObject.GetComponent<SpaceShip>();
+
+        if (spaceship != null && crashMeteorite != null)
+        {
+            spaceship.Direction = new Vector3(1, 1) - spaceship.Direction;
+        }
+    }
+}

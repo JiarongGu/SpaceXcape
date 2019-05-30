60eacc8591c1e7d4a73c5c4feeee6750b2c75456
Jiarong Gu <jiaronggu@hotmail.com>
2019-05-30 17:26:20 +0900

    update level score panel added orbitspining

---
+++ Assets/Scripts/Properties/OrbitSpining.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class OrbitSpining : MonoBehaviour
+{
+    public float speed;
+
+    private float rotation = 0f;
+
+    void Update()
+    {
+        transform.eulerAngles = new Vector3(0, 0, GetRotation());
+    }
+
+    private float GetRotation()
+    {
+        rotation = (rotation + speed * Time.fixedDeltaTime) % 360;
+        return rotation;
+    }
+}
